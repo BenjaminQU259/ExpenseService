87ec95a991d06a57d21c7c8b1751e929bc12d74f
Minghao QU <MQu2@slb.com>
2022-10-26 13:39:52 +0800

    test: add tests Should_return_internal_expense_type_if_project_is_internal, Should_return_expense_type_A_if_project_is_external_and_name_is_project_A, Should_return_expense_type_B_if_project_is_external_and_name_is_project_B, Should_return_other_expense_type_if_project_is_external_and_has_other_name

--- ExpenseServiceTest/ExpenseServiceTest.cs
+++ ExpenseServiceTest/ExpenseServiceTest.cs
@@ -1,48 +1,65 @@
 using Expense.Service;
+using Expense.Service.Exceptions;
+using Expense.Service.Expense;
+using Expense.Service.Projects;
 using Xunit;
 
 namespace Expense.Service.Test
 {
-    public class ExpenseServiceTest
+  public class ExpenseServiceTest
+  {
+    [Fact]
+    public void Should_return_internal_expense_type_if_project_is_internal()
     {
-        [Fact]
-        public void Should_return_internal_expense_type_if_project_is_internal()
-        {
-            // given
-            // when
-            // then
-        }
-
-        [Fact]
-        public void Should_return_expense_type_A_if_project_is_external_and_name_is_project_A()
-        {
-            // given
-            // when
-            // then
-        }
-
-        [Fact]
-        public void Should_return_expense_type_B_if_project_is_external_and_name_is_project_B()
-        {
-            // given
-            // when
-            // then
-        }
-
-        [Fact]
-        public void Should_return_other_expense_type_if_project_is_external_and_has_other_name()
-        {
-            // given
-            // when
-            // then
-        }
-
-        [Fact]
-        public void Should_throw_unexpected_project_exception_if_project_is_invalid()
-        {
-            // given
-            // when
-            // then
-        }
+      // given
+      var project = new Project(ProjectType.INTERNAL, "Internal project");
+      // when
+      var expenseType = ExpenseService.GetExpenseCodeByProjectTypeAndName(project);
+      // then
+      Assert.Equal(ExpenseType.INTERNAL_PROJECT_EXPENSE, expenseType);
     }
+
+    [Fact]
+    public void Should_return_expense_type_A_if_project_is_external_and_name_is_project_A()
+    {
+      // given
+      var project = new Project(ProjectType.EXTERNAL, "Project A");
+      // when
+      var expenseType = ExpenseService.GetExpenseCodeByProjectTypeAndName(project);
+      // then
+      Assert.Equal(ExpenseType.EXPENSE_TYPE_A, expenseType);
+    }
+
+    [Fact]
+    public void Should_return_expense_type_B_if_project_is_external_and_name_is_project_B()
+    {
+      // given
+      var project = new Project(ProjectType.EXTERNAL, "Project B");
+      // when
+      var expenseType = ExpenseService.GetExpenseCodeByProjectTypeAndName(project);
+      // then
+      Assert.Equal(ExpenseType.EXPENSE_TYPE_B, expenseType);
+    }
+
+    [Fact]
+    public void Should_return_other_expense_type_if_project_is_external_and_has_other_name()
+    {
+      // given
+      var project = new Project(ProjectType.EXTERNAL, "Project C");
+      // when
+      var expenseType = ExpenseService.GetExpenseCodeByProjectTypeAndName(project);
+      // then
+      Assert.Equal(ExpenseType.OTHER_EXPENSE, expenseType);
+    }
+
+    [Fact]
+    public void Should_throw_unexpected_project_exception_if_project_is_invalid()
+    {
+      // given
+
+      // when
+
+      // then
+    }
+  }
 }
